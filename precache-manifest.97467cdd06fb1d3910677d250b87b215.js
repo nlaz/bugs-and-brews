76a8dc66ea73556d7d470de9d17fd5b625724ea1
Niko <niko@mlh.io>
2019-12-25 17:11:23 -0500

    Updates

---
+++ precache-manifest.97467cdd06fb1d3910677d250b87b215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b42dc2615e5552903f47c2c767bf20",
+    "url": "/bugs-and-brews/index.html"
+  },
+  {
+    "revision": "ee71f6992a5f7260b5d5",
+    "url": "/bugs-and-brews/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e47712dd9a1c0bf37216",
+    "url": "/bugs-and-brews/static/css/main.0d6c4daa.chunk.css"
+  },
+  {
+    "revision": "ee71f6992a5f7260b5d5",
+    "url": "/bugs-and-brews/static/js/2.4b7835ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bugs-and-brews/static/js/2.4b7835ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e47712dd9a1c0bf37216",
+    "url": "/bugs-and-brews/static/js/main.9170166c.chunk.js"
+  },
+  {
+    "revision": "fc0f8fabbfc3bfa73838",
+    "url": "/bugs-and-brews/static/js/runtime-main.a9623172.js"
+  }
+]);
