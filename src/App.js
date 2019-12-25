84b76cc1f1989d8e1d53df6c2813e270c19a0a49
Niko <niko@mlh.io>
2019-12-25 18:02:55 -0500

    Add scroll banner

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function App() {
   return (
-    <div className="mono bg-washed-blue ma0 ph3" style={{ minHeight: "100vh" }}>
-      <div className="pv6 ma0">
+    <div className="mono bg-washed-blue ma0" style={{ minHeight: "100vh" }}>
+      <marquee className="pv2 bg-black white">
+        Text <strong>718:568:7032</strong> to get on the list.
+      </marquee>
+      <div className="pv6 ma0 ph3">
         <div className="mw6 center v6 ba bw2">
           <h1 className="f2 ma0 bb bw2 pv1 ph2">Bugs & Brews </h1>
           <p className="lh-copy mb4 pv1 ph2">
