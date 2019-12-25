8a488bc033e50db11539cd851a607489dc5a5fc2
Niko <niko@mlh.io>
2019-12-25 17:08:23 -0500

    Create project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mono bg-washed-blue ma0 ph3" style={{ minHeight: "100vh" }}>
+      <div className="pv6 ma0">
+        <div className="mw6 center v6 ba bw2">
+          <h1 className="f2 ma0 bb bw2 pv1 ph2">
+            Bugs & Brews{" "}
+            <span role="img" aria-label="Bug and beer emoji">
+              🐛🍻
+            </span>
+          </h1>
+          <p className="lh-copy mb4 pv1 ph2">
+            Bugs & Brews is a NYC community for creative people to meet up to
+            work on their pet projects. The goal is to give people the space to
+            focus. Everyone must share what they build each night.
+          </p>
+          <p className="pv1 ph2">
+            Text Niko at <strong>718:568:7032</strong> to get on the list.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
